4a6d33ffbdba5818ca03d17b79aecd1c50048908
LAPTOP-JSIJDE3N\Minh <minhtpn@fpt.com.vn>
2023-05-04 16:44:40 +0700

    Fix order controller

--- order-service/src/main/java/com/mingming/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/mingming/orderservice/controller/OrderController.java
@@ -23,14 +23,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    @TimeLimiter(name ="inventory")
-    @Retry(name = "inventory")
-    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
-       return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+//    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+//    @TimeLimiter(name ="inventory")
+//    @Retry(name = "inventory")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+       return orderService.placeOrder(orderRequest);
     }
 
-    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please order after some time!");
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
